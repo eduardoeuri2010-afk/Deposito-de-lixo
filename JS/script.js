9c8d3cfe59a39d301fef36202d59b86c93614814
Eduardo <eduardoeuri2010@gmail.com>
2026-09-17 16:55:01 -0300

    JavaScript

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+console.log("Hola Macaquito")
+const eduardo = "Eduardo Ribeiro"
+console.log(eduardo)
+let pato = "Eae Pae"
+console.log(pato)
+fundodetela = "preto"
+console.log(fundodetela)
+
+if(fundodetela == "Branco")
+document.body.style.backgroundColor = "white"
+else if(fundodetela == "preto")(
+    document.body.style.backgroundColor = "black"
+)
